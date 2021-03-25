4a1ccb5be1c68c67d8e7c2c411d069017fb576b7
Yuto Kitabayashi <stlouis.route66@gmail.com>
2021-03-25 12:00:14 +0900

    Add migirations
    users, services, categoriesテーブルを作成するためのmigrationファイルを追加

--- database/migrations/2021_03_25_023631_create_services_table.php
+++ database/migrations/2021_03_25_023631_create_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateServicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('user_id')->index();
+            $table->string('name', 100);
+            $table->unsignedInteger('price');
+            $table->foreignId('category_id')->index();
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('services');
     }
 }
